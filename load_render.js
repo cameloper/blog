e48422aafc0346c1be7f135784827a32ca31aed6
Ihsan Batughan Yilmaz <cameloper@cameloper.com>
2022-07-31 22:50:30 +0200

    Added loading of posts and indexing

--- load_render.js
+++ load_render.js
@@ -8,4 +8,31 @@ if (params.has('p')) {
   document.getElementById("content").innerHTML = `<md-block src=\"txt/${postId}.md\">Failed to load post with id ${postId} :(`;
 } else {
   console.log("no post id found in url");
+  console.log("trying to load list of posts");
+  loadPosts();
 }
+
+async function loadPosts() {
+  console.log('fetching posts');
+  fetch('posts.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("HTTP error " + response.status);
+    }
+    return response.json();
+  })
+  .then(json => {
+    let links = json.map(parsePost);
+    let indexString = "<ul>\n" + links.join('\n') + "\n</ul>";
+    document.getElementById("content").innerHTML = indexString;
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+}
+
+function parsePost(post) {
+  return `<li><span class="postindex">${post.date}</span>&ensp;<a href="index.html?p=${post.id}">${post.title}</a></li>`;
+
+}
+
